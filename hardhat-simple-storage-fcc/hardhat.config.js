c519c78185c1cbd8a43e213b553225139939ca06
Mishkat Mustafid <mishkatmustafid@gmail.com>
2022-11-22 09:32:35 +0600

    Lesson 6: complete

--- hardhat-simple-storage-fcc/hardhat.config.js
+++ hardhat-simple-storage-fcc/hardhat.config.js
@@ -1,6 +1,45 @@
-require("@nomicfoundation/hardhat-toolbox");
+require("@nomicfoundation/hardhat-toolbox")
+require("@nomiclabs/hardhat-etherscan")
+require("./tasks/block-number")
+require("dotenv").config()
+require("hardhat-gas-reporter")
+require("solidity-coverage")
+require("@nomiclabs/hardhat-waffle")
 
 /** @type import('hardhat/config').HardhatUserConfig */
+
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || ""
+const GOERLI_RPC_URL =
+  process.env.GOERLI_RPC_URL ||
+  "https://eth-goerli.alchemyapi.io/v2/your-api-key"
+const PRIVATE_KEY =
+  process.env.PRIVATE_KEY ||
+  "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80"
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || ""
+
 module.exports = {
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {},
+    goerli: {
+      url: GOERLI_RPC_URL,
+      accounts: [PRIVATE_KEY],
+      chainId: 5,
+    },
+    localhost: {
+      url: "http://localhost:8545",
+      chainId: 31337,
+    },
+  },
   solidity: "0.8.9",
-};
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY,
+  },
+  gasReporter: {
+    enabled: true,
+    currency: "USD",
+    outputFile: "gas-report.txt",
+    noColors: true,
+    coinmarketcap: COINMARKETCAP_API_KEY,
+  },
+}
