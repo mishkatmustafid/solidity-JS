422a5abbd2eb30e5f2d1b29004c0583ac5f2ca58
Mishkat Mustafid <mishkatmustafid@gmail.com>
2022-11-26 15:19:26 +0600

    config update

--- hardhat-simple-storage-fcc/hardhat.config.js
+++ hardhat-simple-storage-fcc/hardhat.config.js
@@ -14,7 +14,7 @@ const GOERLI_RPC_URL =
   "https://eth-goerli.alchemyapi.io/v2/your-api-key"
 const PRIVATE_KEY =
   process.env.PRIVATE_KEY ||
-  "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80"
+  "PRIVATE_KEY_HERE"
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || ""
 
 module.exports = {
--- hardhat-simple-storage-fcc/hardhat.config.ts
+++ hardhat-simple-storage-fcc/hardhat.config.ts
@@ -15,9 +15,7 @@ const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || ""
 const GOERLI_RPC_URL =
   process.env.GOERLI_RPC_URL ||
   "https://eth-goerli.alchemyapi.io/v2/your-api-key"
-const PRIVATE_KEY =
-  process.env.PRIVATE_KEY ||
-  "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80"
+const PRIVATE_KEY = process.env.PRIVATE_KEY || "PRIVATE_KEY_HERE"
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || ""
 
 module.exports = {
