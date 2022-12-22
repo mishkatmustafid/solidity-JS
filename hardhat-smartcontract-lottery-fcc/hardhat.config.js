e0f954db7ba197cbb957966bd1adf5137b3848ae
Mishkat Mustafid <mishkatmustafid@gmail.com>
2022-12-22 08:51:17 +0600

    hardhat config

--- hardhat-smartcontract-lottery-fcc/hardhat.config.js
+++ hardhat-smartcontract-lottery-fcc/hardhat.config.js
@@ -1,12 +1,79 @@
-require("@nomiclabs/hardhat-waffle");
-require("@nomiclabs/hardhat-etherscan");
-require("hardhat-deploy");
-require("solidity-coverage");
-require("hardhat-gas-reporter");
-require("hardhat-contract-sizer");
-require("dotenv").config();
+require("@nomiclabs/hardhat-waffle")
+require("@nomiclabs/hardhat-etherscan")
+require("hardhat-deploy")
+require("solidity-coverage")
+require("hardhat-gas-reporter")
+require("hardhat-contract-sizer")
+require("dotenv").config()
 
-/** @type import('hardhat/config').HardhatUserConfig */
+const MAINNET_RPC_URL =
+  process.env.MAINNET_RPC_URL ||
+  process.env.ALCHEMY_MAINNET_RPC_URL ||
+  "https://eth-mainnet.alchemyapi.io/v2/your-api-key"
+const GOERLI_RPC_URL =
+  process.env.GOERLI_RPC_URL || "https://eth-goerli.alchemyapi.io/v2/your-api-key"
+const POLYGON_MAINNET_RPC_URL =
+  process.env.POLYGON_MAINNET_RPC_URL || "https://polygon-mainnet.alchemyapi.io/v2/your-api-key"
+const PRIVATE_KEY = process.env.PRIVATE_KEY || "0x"
+
+// Your API key for Etherscan, obtain one at https://etherscan.io/
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "Your etherscan API key"
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || "You coinmarketcap API key"
+const POLYGONSCAN_API_KEY = process.env.POLYGONSCAN_API_KEY || "Your polygonscan API key"
+const REPORT_GAS = process.env.REPORT_GAS || false
 module.exports = {
-  solidity: "0.8.7",
-};
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      // // If you want to do some forking, uncomment this
+      // forking: {
+      //   url: MAINNET_RPC_URL
+      // },
+      blockConfirmations: 1,
+      chainId: 31337,
+    },
+    localhost: { chainId: 31337 },
+    goerli: {
+      url: GOERLI_RPC_URL,
+      accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
+      // accounts: {
+      //   mnemonic: MNEMONIC,
+      // }
+      saveDeployments: true,
+      blockConfirmations: 5,
+      chainId: 5,
+    },
+    mainnet: {
+      url: MAINNET_RPC_URL,
+      accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
+      //   accounts: {
+      //     mnemonic: MNEMONIC,
+      //   },
+      saveDeployments: true,
+      chainId: 1,
+    },
+    polygon: {
+      url: POLYGON_MAINNET_RPC_URL,
+      accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
+      saveDeployments: true,
+      chainId: 137,
+    },
+  },
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.7",
+      },
+      {
+        version: "0.4.24",
+      },
+    ],
+  },
+  mocha: {
+    timeout: 500000, // 500 seconds max for running tests
+  },
+  namedAccounts: {
+    deployer: { default: 0 },
+    player: { default: 1 },
+  },
+}
