7f04a57b4afb1a89c6b22b1cfdaa75784ddbc9a2
Mishkat Mustafid <mishkatmustafid@gmail.com>
2023-01-13 19:23:17 +0600

    Additional config added

--- hardhat-smartcontract-lottery-fcc/hardhat.config.js
+++ hardhat-smartcontract-lottery-fcc/hardhat.config.js
@@ -59,6 +59,44 @@ module.exports = {
       chainId: 137,
     },
   },
+  etherscan: {
+    // yarn hardhat verify --network <NETWORK> <CONTRACT_ADDRESS> <CONSTRUCTOR_PARAMETERS>
+    apiKey: {
+      goerli: ETHERSCAN_API_KEY,
+      polygon: POLYGONSCAN_API_KEY,
+    },
+    customChains: [
+      {
+        network: "goerli",
+        chainId: 5,
+        urls: {
+          apiURL: "https://api-goerli.etherscan.io/api",
+          browserURL: "https://goerli.etherscan.io",
+        },
+      },
+    ],
+  },
+  gasReporter: {
+    // enabled: REPORT_GAS,
+    enabled: false,
+    currency: "USD",
+    outputFile: "gas-report.txt",
+    noColors: true,
+    // coinmarketcap: process.env.COINMARKETCAP_API_KEY,
+  },
+  contractSizer: {
+    runOnCompile: false,
+    only: ["Raffle"],
+  },
+  namedAccounts: {
+    deployer: {
+      default: 0, // here this will by default take the first account as deployer
+      1: 0, // similarly on mainnet it will take the first account as deployer. Note though that depending on how hardhat network are configured, the account 0 on one network can be different than on another
+    },
+    player: {
+      default: 1,
+    },
+  },
   solidity: {
     compilers: [
       {
@@ -71,9 +109,5 @@ module.exports = {
   },
   mocha: {
     timeout: 500000, // 500 seconds max for running tests
-  },
-  namedAccounts: {
-    deployer: { default: 0 },
-    player: { default: 1 },
   },
 }
