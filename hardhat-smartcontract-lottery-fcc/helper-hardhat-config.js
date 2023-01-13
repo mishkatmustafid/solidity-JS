8b3bd7a1041cfc84fc7d70dfdbdf064836097164
Mishkat Mustafid <mishkatmustafid@gmail.com>
2023-01-13 17:58:50 +0600

    subscriptionId updated

--- hardhat-smartcontract-lottery-fcc/helper-hardhat-config.js
+++ hardhat-smartcontract-lottery-fcc/helper-hardhat-config.js
@@ -7,7 +7,7 @@ const networkConfig = {
   },
   31337: {
     name: "localhost",
-    subscriptionId: "588",
+    subscriptionId: "1",
     gasLane: "0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc", // 30 gwei
     keepersUpdateInterval: "30",
     raffleEntranceFee: ethers.utils.parseEther("0.01"), // 0.01 ETH
@@ -15,7 +15,7 @@ const networkConfig = {
   },
   5: {
     name: "goerli",
-    subscriptionId: "6926",
+    subscriptionId: "7722",
     gasLane: "0x79d3d8832d904592c0bf9818b621522c988bb8b0c05cdc3b15aea1b6e8db0c15", // 30 gwei
     keepersUpdateInterval: "30",
     raffleEntranceFee: ethers.utils.parseEther("0.01"), // 0.01 ETH
